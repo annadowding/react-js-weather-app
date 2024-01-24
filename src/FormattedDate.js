6b152ca27fe9843c42e7a074dd3ef7adbb3fe15c
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-24 12:22:11 +0000

    Sunrise and sunset

---
+++ src/FormattedDate.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+export default function FormattedDate (props) {
+    let date = props.date;
+    let dateToday = date.getDate();
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours=`0${date.getHours()}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes <10) {
+        minutes = `0${date.getMinutes()}`
+    }
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    let day = days[date.getDay()];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month = months[date.getMonth()];
+    
+    return (
+        <div className="FormattedDate">
+        <div>GMT: {hours}:{minutes}</div> 
+        <div>{day} {dateToday} {month}</div>
+        </div>
+    )
+
+    }
+
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,9 @@
 import React, {useState} from "react";
 import "./Weather.css";
 import axios from "axios";
-import FormattedSunrise from "./FormattedSunrise.js"
+import FormattedDate from "./FormattedDate.js";
+import Sunrise from "./Sunrise.js";
+import Sunset from "./Sunset.js";
 
 export default function Weather (props) {
     const[ready, setReady] = useState(false);
@@ -9,15 +11,21 @@ export default function Weather (props) {
     const[weather, setWeather] = useState({});
 
     function handleWeatherData (response) {
-        console.log(response.data);
-        setReady(true);
-        setWeather({
-        sunrise: new Date((response.data.sys.sunrise + response.data.timezone)*1000),
-        temperature:response.data.main.temp,
-        humidity:response.data.main.humidity,
-        wind: response.data.wind.speed,
-        city: response.data.name,
-    })
+      console.log(response.data);
+      setReady(true);
+      setWeather({
+      date: new Date(response.data.dt * 1000),
+      sunrise: new Date(
+      (response.data.sys.sunrise + response.data.timezone) * 1000
+      ),
+      sunset: new Date(
+      (response.data.sys.sunset + response.data.timezone) * 1000
+      ),
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+        });
   }
 
       function handleSubmit (event) {
@@ -38,22 +46,23 @@ export default function Weather (props) {
     if (ready) {
       return (
         <div className="Weather">
-         <form onSubmit={handleSubmit}>
-           <input type="search" onChange={handleCityChange}/>
-           <input type="submit" />
-         </form>
-         <div>
-          <div>{weather.city}</div>
-           <FormattedSunrise date={weather.sunrise}/>
-           <div>⛅</div>
-           <div>{Math.round(weather.temperature)}℃</div>
-           <div>Humidity: {weather.humidity}%</div>
-           <div>Wind: {weather.wind}km/h</div>
-           <div>Forecast: ☀️🌦️🌥️🌤️☀️</div>
-         </div>
-       </div>
-    
-    );
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={handleCityChange} />
+            <input type="submit" />
+          </form>
+          <div>
+            <div>{weather.city}</div>
+            <FormattedDate date={weather.date} />
+            <Sunrise sunrise={weather.sunrise} />
+            <Sunset sunset={weather.sunset} />
+            <div>⛅</div>
+            <div>{Math.round(weather.temperature)}℃</div>
+            <div>Humidity: {weather.humidity}%</div>
+            <div>Wind: {weather.wind}km/h</div>
+            <div>Forecast: ☀️🌦️🌥️🌤️☀️</div>
+          </div>
+        </div>
+      );
   } else {
 handleResponse();
         return (
