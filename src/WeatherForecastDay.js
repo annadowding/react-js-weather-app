9ebeefe3134674e5e0b1da959d746f83a5e0ece2
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-25 11:32:28 +0000

    Forecast day name

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 export default function WeatherForecastDay (props) {
+
+    let day = new Date(props.data.sunrise*1000);
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let today = days[day.getDay()];
+ 
     return (
         <div>
+            <div>
+                {today}
+            </div>
             <div>
                 {Math.round(props.data.temp.day)}℃
             </div>
